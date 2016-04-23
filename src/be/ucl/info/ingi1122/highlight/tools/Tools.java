816076e56d637063b292d45647ffc37ef194d37b
admin <admin@admins-mbp.home>
2016-04-23 21:32:53 +0200

    Salut

---
+++ src/be/ucl/info/ingi1122/highlight/tools/Tools.java
@@ -0,0 +1,13 @@
+package be.ucl.info.ingi1122.highlight.tools;
+
+public class Tools {
+
+	public static Portion[] quoiSurligner(char[] texte, char[][] mots) {
+		return new Portion[0];
+	}
+	
+	public static boolean correspond(char[] texte, char[][] mots) {
+		return true;
+	}
+	
+}
