a1b45c2e956375af112ee92141cd93020a69f359
miserentant <merlin.iserentant@student.uclouvain.be>
2016-04-24 13:46:15 +0200

    Update Tools.java

--- src/be/ucl/info/ingi1122/highlight/tools/Tools.java
+++ src/be/ucl/info/ingi1122/highlight/tools/Tools.java
@@ -1,9 +1,21 @@
 package be.ucl.info.ingi1122.highlight.tools;
 
 public class Tools {
+	
+	public Portion[] tabportion;
+	public PortionSet portionset;
 
 	public static Portion[] quoiSurligner(char[] texte, char[][] mots) {
-		return new Portion[0];
+		portionset = new PortionSet();
+		boolean bool = correspond(texte, mots);
+		if(!bool){
+			return null
+		}
+		tabportion= new Portion[portionset.number];
+		for(int i =0;i<portionset.number;i++){
+			tabportion[i]=portionset.tab.get(i);
+		}
+		return tabportion;
 	}
 	
 	public static boolean correspond(char[] texte, char[][] mots) {
