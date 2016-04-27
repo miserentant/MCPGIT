c9480bb5fd786992f588837e783f80d6508601c1
lougerard <louis.gerard@student.uclouvain.be>
2016-04-27 10:14:47 +0200

    Update Tools.java

--- src/be/ucl/info/ingi1122/highlight/tools/Tools.java
+++ src/be/ucl/info/ingi1122/highlight/tools/Tools.java
@@ -20,18 +20,15 @@ public class Tools {
 	}
 	/*
 	 * @pre prend un texte et une liste de mots en parametre
-	 * @post renvoie true si tout les mots se trouve dans le texte
+	 * @post renvoie true si un mot de mots se trouve dans le texte
 	 */
 	public static boolean correspond(char[] texte, char[][] mots) {
 		boolean trouve = false;
-		int caractereTrouve = 0;
 		int motTrouve = 0;
 		for (int i=0; i< mots.lenght()-1; i++){
 			int taille = mots[i].lenght();
-			if (oneMath(texte, mots[i])){motTrouve ++;}
-			if (motTrouve == taille) {caractereTrouve++;}
-		}
-		if (caractereTrouve == mots.lenght())
+			if (oneMath(texte, mots[i])){motTrouve ++;}		}
+		if (motTrouve >= 1 && motTrouve < taille)
 		{
 			trouve = true;
 		}
@@ -47,10 +44,20 @@ public class Tools {
 		int tailleM = mot.lenght();
 		int tailleT = texte.lenght();
 		int caractereTrouve = 0;
-		for (int i=0; i<(tailleT-tailleM-1)) {
-				
+		for (int i=0; i<(tailleT-tailleM-1) && match == false; i++) {
+			if(texte[i] == mot[0]) {
+				match = compare(i,texte,mot);
+			}
 		}
 		return match;
 	}
+	
+	/*
+	 * @pre prend un texte, une position p dans ce texte et un mot en argument
+	 * @post renvoit true si le mot se trouve a la position p du texte
+	 */
+	 public boolean compare(int position, char[] texte, char[] mot){
+	 	
+	 }
 	
 }
