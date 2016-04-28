438fae4dae32ef0d66f53a3eaa92d3b632d2cc63
miserentant <merlin.iserentant@student.uclouvain.be>
2016-04-28 21:43:30 +0200

    Update Tools.java

--- src/be/ucl/info/ingi1122/highlight/tools/Tools.java
+++ src/be/ucl/info/ingi1122/highlight/tools/Tools.java
@@ -1,56 +1,42 @@
 package be.ucl.info.ingi1122.highlight.tools;
 
 
+
 public class Tools {
-	
-	//public static Portion[] tabportion;
-	//public static PortionSet portionset;
 
 	public static Portion[] quoiSurligner(char[] texte, char[][] mots) {
-		
-		System.out.println(new String(texte));
-		PortionSet portionset = portioncreate(texte,mots);
-		
-	
-		Portion[] tabportion = new Portion[portionset.getNumberElem()];
-		for(int i =0; i<portionset.getNumberElem()-1; i++){
-			try {
-				tabportion[i] = portionset.getElem(i);
-			}
-			catch (ArrayIndexOutOfBoundsException e) {System.out.println("cherche élément en dehors du tableau");}
-		}
-		return tabportion;
+        
+
+
+      
+        PortionSet portionSet = matchSurligner(texte,mots);
+           
+
+
+        return portionSet.toTab();
 	}
-	public static PortionSet portioncreate(char [] texte, char[][] mots){
-		PortionSet portionset = new PortionSet();
-		int i=0;
-		int j;
-		while(i<texte.length){
-			j=0;
-			while(j<mots.length){
-				
-				if(j<texte.length-mots[j].length-1){
-					if(compare(i,texte,mots[j])){
-						portionset.Add(new Portion(i,i+mots[j].length));
-				
-					}
-					
-				}
-				j++;
-			}
-			i++;
-		}
-		return portionset;
+
+    public  static  PortionSet matchSurligner(char[] texte, char[][] mots){
+        PortionSet result  = new PortionSet();
+        int k;
+        for(k=0;k<texte.length;k++){
+        	int i=0;
+        	while (i< mots.length){
+        		if(k <= texte.length-mots[i].length){
+        			if(compare(k,texte,mots[i])){
+        				result.Add(new Portion(k,k+mots[i].length));
+        			}
+        		}
+        		i++;
+        	}
+        }
+        return result;
+    }
 	
-	}
-	/*
-	 * @pre prend un texte et une liste de mots en parametre
-	 * @post renvoie true si tous les mots se trouve dans le texte
-	 */
-	
+
 	public static boolean correspond(char[] texte, char[][] mots) {
 
-		for (int i=0; i< mots.length-1; i++){
+		for (int i=0; i< mots.length; i++){
 			if ( oneMatch(texte, mots[i]) == false){return false;}
 		}
 		
@@ -60,7 +46,7 @@ public class Tools {
 	public static boolean oneMatch(char[] texte, char[] mot){
 		 int i=0;
 	        boolean match=false;
-	        while(i < texte.length-mot.length-1 && !match){
+	        while(i <= texte.length-mot.length && !match){
 	            match= compare(i,texte,mot);
 	            i++;
 	        }
