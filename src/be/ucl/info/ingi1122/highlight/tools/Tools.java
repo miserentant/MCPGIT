f47f55d3710a18562085ae49ed9e79fafe874d65
miserentant <merlin.iserentant@student.uclouvain.be>
2016-04-28 21:04:05 +0200

    Update Tools.java

--- src/be/ucl/info/ingi1122/highlight/tools/Tools.java
+++ src/be/ucl/info/ingi1122/highlight/tools/Tools.java
@@ -1,17 +1,21 @@
 package be.ucl.info.ingi1122.highlight.tools;
+import com.sun.jmx.snmp.agent.SnmpUserDataFactory;
+
+import javax.sound.sampled.Port;
 
 public class Tools {
 	
-	public Portion[] tabportion;
-	public PortionSet portionset;
+	public static Portion[] tabportion;
+	public static PortionSet portionset;
 
 	public static Portion[] quoiSurligner(char[] texte, char[][] mots) {
-		portionset = new PortionSet();
+		
 		boolean bool = correspond(texte, mots);
 		if(!bool){
 			System.out.println("No match found");
 			return null;
 		}
+		portionset = portioncreate(texte,mots);
 		tabportion = new Portion[portionset.getNumberElem()];
 		for(int i =0; i<portionset.getNumberElem()-1; i++){
 			try {
@@ -21,55 +25,60 @@ public class Tools {
 		}
 		return tabportion;
 	}
+	public static PortionSet portioncreate(char [] texte, char[][] mots){
+		PortionSet portionset = new PortionSet();
+		int i,j;
+		for(i=0;i<texte.length;i++){
+			for(j=0;j<mots.length;j++){
+				if(compare(i,texte,mots[j])){
+					portionset.Add(new Portion(i,i+mots[j].length));
+				}
+			}
+		}
+		return portionset;
+	
+	}
 	/*
 	 * @pre prend un texte et une liste de mots en parametre
 	 * @post renvoie true si tous les mots se trouve dans le texte
 	 */
+	
 	public static boolean correspond(char[] texte, char[][] mots) {
-		boolean trouve = false;
-		int motTrouve = 0;
-		int tailleM = mots[0].lenght();
-		for (int i=0; i< mots.lenght()-1; i++){
-			if (oneMath(texte, mots[i]) == true){motTrouve ++;}
+
+		for (int i=0; i< mots.length-1; i++){
+			if ( oneMatch(texte, mots[i]) == false){return false;}
 		}
-		if (motTrouve == tailleM)
-		{
-			trouve = true;
-		}
-		return trouve;
+		
+		return true;
 	}
-	/*
-	 * @pre prend un texte et un mots a chercher en parametre
-	 * @post renvoie true si le mot se trouve dans le texte
-	*/
-	public boolean oneMatch(char[] texte, char[] mot){
-		boolean match = false;
-		int tailleM = mot.lenght();
-		int tailleT = texte.lenght();
-		for (int i=0; i<(tailleT-tailleM-1) && match == false; i++) {
-			if(texte[i] == mot[0]) {
-				match = compare(i,texte,mot);
-			}
-		}
-		return match;
+
+	public static boolean oneMatch(char[] texte, char[] mot){
+		 int i=0;
+	        boolean match=false;
+	        while(i < texte.length-mot.length-1 && !match){
+	            match= compare(i,texte,mot);
+	            i++;
+	        }
+	        return match;
 	}
 	
-	/*
-	 * @pre prend un texte, une position p dans ce texte et un mot en argument
+
+	 /* @pre prend un texte, une position p dans ce texte et un mot en argument
 	 * @post renvoit true si le mot se trouve a la position p du texte
 	 */
-	 public boolean compare(int position, char[] texte, char[] mot){
-	 	boolean idem = false;
-	 	boolean dansMot = true;
-	 	int caractereTrouve = 0;
-	 	int tailleM = mot.lenght();
-	 	int tailleT = texte.lenght();
-	 	for (int i = position; i<(tailleT-tailleM-1) && i<tailleM && dansMot == true; i++){
-	 		if (texte[i] == mot[i]) {caractereTrouve ++;}
-	 		else {dansMot = false;}
-	 	}
-	 	if (caractereTrouve == tailleM) {idem = true;}
-	 	return idem;
-	 }
+    public static boolean compare(int position, char[] texte, char[] mot){
+        int j=0;
+        while (j< mot.length && texte[position+j]==mot[j]){
+            j=j+1;
+        }
+        if (j == mot.length){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 	
 }
